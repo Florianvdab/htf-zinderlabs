ea18d6a6b478677215672bcc556b5a7b80dbb8cf
florian.vandenabeele <florian.vandenabeele@student.howest.be>
2020-11-24 12:02:08 +0100

    await weg

--- src/api.js
+++ src/api.js
@@ -1,67 +1,77 @@
-const api = function() {
-    const baseURL = `https://htf2020.zinderlabs.com/api`;
+const api = function () {
+  const baseURL = `https://htf2020.zinderlabs.com/api`;
 
-    function getJson(url) {
-        return fetch(url, {
-            'headers': {
-                'Authorization': `Bearer ${getTokenFromCookie()}`
-            },
-            'method': 'get'
-        })
-            .then(res => res.json())
-            .catch(console.log)
-        }
+  function getJson(url) {
+    return fetch(url, {
+      headers: {
+        Authorization: `Bearer ${getTokenFromCookie()}`,
+      },
+      method: "get",
+    })
+      .then((res) => res.json())
+      .catch(console.log);
+  }
 
-    function postData(url, data) {
-        return fetch(url, {
-            'headers': {
-                'Authorization': `Bearer ${getTokenFromCookie()}`
-            },
-            'method': 'post',
-            'body': JSON.stringify(data),
-          })
-            .then(res => res.json())
-            .catch(console.log)
-    }
+  function postData(url, data) {
+    return fetch(url, {
+      headers: {
+        Authorization: `Bearer ${getTokenFromCookie()}`,
+      },
+      method: "post",
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .catch(console.log);
+  }
 
-    function getTokenFromCookie() {
-        if (!!getCookie("token") && JSON.parse(getCookie("token")).hasOwnProperty("access_token")) {
-            return JSON.parse(getCookie("token")).access_token;
-        } else return null;
-    }
+  function getTokenFromCookie() {
+    if (
+      !!getCookie("token") &&
+      JSON.parse(getCookie("token")).hasOwnProperty("access_token")
+    ) {
+      return JSON.parse(getCookie("token")).access_token;
+    } else return null;
+  }
 
-    function getCookie(name) {
-        let matches = document.cookie.match(new RegExp(
-          "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-        ));
-        return matches ? decodeURIComponent(matches[1]) : undefined;
-      }
+  function getCookie(name) {
+    let matches = document.cookie.match(
+      new RegExp(
+        "(?:^|; )" +
+          name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+          "=([^;]*)"
+      )
+    );
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
 
-    function setCookie(name, value, ttl) {
-        let d = new Date();
-        d.setTime(d.getTime() + ttl);
-        document.cookie = `${name}=${value};expires=${d.toLocaleString()};path=/`
-    }
+  function setCookie(name, value, ttl) {
+    let d = new Date();
+    d.setTime(d.getTime() + ttl);
+    document.cookie = `${name}=${value};expires=${d.toLocaleString()};path=/`;
+  }
 
-    return {
-        authorize(user, pass) {
-            let res = await postData(`${baseURL}/login`, {username: user, password: pass});
-            setCookie("token", res, res.expires_in);
-        },
-        getDatacenters() {
-            return getJson(`${baseURL}/datacenters`);
-        },
-        getDatacenterErrors(id) {
-            return getJson(`${baseURL}/datacenters/${id}/errors`);
-        },
-        isolateDatacenter(id) {
-            return postData(`${baseURL}/datacenters/${id}/isolate`, null)
-        },
-        getCriticalErrors() {
-            return getJson(`${baseURL}/critical_errors`);
-        },
-        setCriticalErrors(value) {
-            return postData(`${baseURL}/critical_errors`, {value: value});
-        }
-    }
-}
+  return {
+    authorize(user, pass) {
+      let res = postData(`${baseURL}/login`, {
+        username: user,
+        password: pass,
+      });
+      setCookie("token", res, res.expires_in);
+    },
+    getDatacenters() {
+      return getJson(`${baseURL}/datacenters`);
+    },
+    getDatacenterErrors(id) {
+      return getJson(`${baseURL}/datacenters/${id}/errors`);
+    },
+    isolateDatacenter(id) {
+      return postData(`${baseURL}/datacenters/${id}/isolate`, null);
+    },
+    getCriticalErrors() {
+      return getJson(`${baseURL}/critical_errors`);
+    },
+    setCriticalErrors(value) {
+      return postData(`${baseURL}/critical_errors`, { value: value });
+    },
+  };
+};
